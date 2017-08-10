88cf9380a4734199701a8e508fdb51279718f557
jrreed83 <joeyrreed@gmail.com>
2017-08-09 20:54:28 -0700

    jrr

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -4,22 +4,24 @@ defmodule RotationalCipher do
     def transform_char(x, shift) do
         case x do
             x when (?a <= x) and (x <= ?z)  ->
-                xx = x + shift - ?a 
-                xx = rem(xx, 26)
-                xx = xx + ?a 
-                xx
+                if x + shift > ?z do 
+                    ?a + (x + shift - ?z - 1)
+                else
+                    x + shift 
+                end
             x when (?A <= x) and (x <= ?Z) -> 
-                xx = x + shift - ?A 
-                xx = rem(xx, 26)
-                xx = xx + ?A 
-                xx
+                if x + shift > ?Z do 
+                    ?A + (x + shift - ?Z - 1)
+                else
+                    x + shift 
+                end
             _ -> 
                 x
         end   
     end
 
     def rotate(text, shift) do
-        
+
         text |> to_charlist   # Convert to character list
              |> Enum.map(fn(x) -> transform_char(x, shift) end) # Rotate each character
              |> to_string # Convert back to string  
