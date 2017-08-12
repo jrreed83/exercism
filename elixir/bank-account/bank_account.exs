6f5cba4a8f6a75e07c649fb5d5479f93a4749a51
jrreed83 <joeyrreed@gmail.com>
2017-08-12 13:20:13 -0700

    jrr

--- elixir/bank-account/bank_account.exs
+++ elixir/bank-account/bank_account.exs
@@ -1,27 +1,47 @@
 defmodule BankAccount do
     def open_account() do
-        spawn(fn() -> loop(0) end)
+        spawn(fn() -> loop(%{balance: 0, state: :open}) end)
     end
 
-    def loop(balance) do
+    def loop(%{balance: b, state: s}) do
         receive do
             {sender, :balance} ->
-                send(sender,{self(), balance}) 
-                loop(balance)
-            {_, :update, amt} ->
-                new_balance = balance+amt
-                loop(new_balance)
+                if s == :closed do 
+                    send(sender, {self(), :error, :account_closed})
+                else
+                    send(sender, {self(), :ok, b}) 
+                end
+                loop(%{balance: b, state: s})
+            {sender, :update, amt} ->
+                if s == :closed do 
+                    send(sender, {self(), :error, :account_closed})   
+                    loop(%{balance: b, state: s})
+                else
+                    send(sender, {self(), :ok,})                       
+                    loop(%{balance: b+amt, state: s})
+                end
+            {_, :close} -> 
+                loop(%{balance: b, state: :closed})
         end
     end
 
     def balance(account) do
         send(account, {self(), :balance})
         receive do
-            {_, balance} -> balance
+            {_, :ok,    balance} -> balance
+            {_, :error, :account_closed} -> {:error, :account_closed}
         end    
     end
 
     def update(account, amt) do
         send(account, {self(), :update, amt}) 
+        receive do
+            {_, :error, :account_closed} -> {:error, :account_closed}
+            {_, :ok} -> :ok
+        end         
+    end
+
+    def close_account(account) do
+        send(account, {self(), :close})
     end
 end
