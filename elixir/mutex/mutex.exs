aa0353684a93bb8bf44db1297f3003198faf9f09
jrreed83 <joeyrreed@gmail.com>
2017-08-12 14:29:49 -0700

    jrr

--- elixir/mutex/mutex.exs
+++ elixir/mutex/mutex.exs
@@ -31,4 +31,44 @@ defmodule Mutex do
     end
 end
 
+defmodule Semaphore do
+    def create(n) do 
+        spawn(fn() -> loop(n) end)
+    end
+
+    def release(sem) do
+        send(sem, {self(), :release}) 
+    end
+
+    def acquire(sem) do 
+        send(sem, {self(), :acquire})
+        receive do 
+            {_, :ok} -> :ok
+        end
+    end
+
+    def loop(n) do
+        if n == -1 do
+            receive do
+                {sender, :release} ->
+                    send(sender, {self(), :ok}) 
+                    loop(0)
+            end
+        else
+            receive do
+                {sender, :release} -> 
+                    send(sender, {self(), :ok})
+                    loop(n+1)
+                {sender, :acquire} ->
+                    if n == 0 do
+                        loop(-1)
+                    else
+                        send(sender, {self(), :ok})
+                        loop(n-1) 
+                    end
+            end
+        end
+    end
+end
+
 
