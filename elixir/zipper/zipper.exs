860fada72819f2df775b1b143fbf89479cf2d988
jrreed83 <joeyrreed@gmail.com>
2017-08-17 17:27:52 -0700

    jrr

---
+++ elixir/zipper/zipper.exs
@@ -0,0 +1,22 @@
+defmodule Zipper do
+    def from_list([h|t]) do
+        [prev: [], focus: h, next: t]
+    end
+
+    def next([prev: p, focus: f, next: n]) do
+        [h|t] = n
+        [prev: [f|p], focus: h, next: t]    
+    end
+
+    def prev([prev: [h|t], focus: f, next: n]) do
+        [prev: [t], focus: h, next: [f|n]]    
+    end
+
+    def prev([prev: [], focus: f, next: n]) do
+        [prev: [], focus: f, next: n]    
+    end
+
+    def to_list([prev: p, focus: f, next: n]) do
+        Enum.reverse(p) ++ [f] ++ n    
+    end
+end
