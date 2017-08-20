f9da1c8072cb84193a8635db531ab249854b336d
jrreed83 <joeyrreed@gmail.com>
2017-08-20 10:32:40 -0700

    jrr

---
+++ elixir/huffman-code/huffman.exs
@@ -0,0 +1,37 @@
+defmodule Huffman do
+    
+    def merge({t1, p1}, {t2, p2}) do
+        if p1 < p2 do
+            {[t1, t2], p1+p2}
+        else
+            {[t2, t1], p1+p2}
+        end
+    end
+
+end
+
+defmodule PriorityQueue do
+    def create() do
+        []
+    end
+
+    def add(queue, node) do
+        add_p(queue, node, [])    
+    end
+
+    # We've gone though everything, the proposed node is
+    # the lowest priority
+    defp add_p([], node, front) do
+        front ++ [node]        
+    end
+
+    defp add_p([h|t], node, front) do
+        
+        # node is lower priority, so continue
+        add_p(t, node, front ++ [h])
+
+        # node is higher priority than h, so insert
+        front ++ [node] ++ [h|t] 
+    end
+
+end
--- elixir/zipper/zipper.exs
+++ elixir/zipper/zipper.exs
@@ -8,6 +8,10 @@ defmodule Zipper do
         [prev: [f|p], focus: h, next: t]    
     end
 
+    def next([prev: p, focus: f, next: []]) do
+        [prev: p, focus: f, next: []]    
+    end
+
     def prev([prev: [h|t], focus: f, next: n]) do
         [prev: [t], focus: h, next: [f|n]]    
     end
@@ -18,5 +22,13 @@ defmodule Zipper do
 
     def to_list([prev: p, focus: f, next: n]) do
         Enum.reverse(p) ++ [f] ++ n    
+    end
+
+    def set_value([prev: p, focus: _, next: n], val) do
+        [prev: p, focus: val, next: n]
+    end
+
+    def value([prev: p, focus: f, next: n]) do
+        f
     end
 end
