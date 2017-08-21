ccc765cede426e13d10ec0274c49359881e529db
jrreed83 <joeyrreed@gmail.com>
2017-08-21 06:16:49 -0700

    jrr

--- elixir/huffman-code/huffman.exs
+++ elixir/huffman-code/huffman.exs
@@ -11,13 +11,27 @@ defmodule Huffman do
     def tree(queue) do
         case length(queue) do
             1 -> 
+                # We want to stop when we really have a tree
                 queue
             _ ->
+                # Pop the two least frequently ocurring symbols from the queue
                 [n1, n2 | t] = queue
+                # Merge the two symbols
                 nn = merge(n1, n2)
+                # Add the newly merged symbol to the reduced queue
                 queue = PriorityQueue.add(t, nn)
+                # Continue
                 tree(queue)
         end    
+    end
+
+    def unwrap({tree, pattern}) do
+        case tree do
+            [left, right] ->
+                unwrap({left, pattern <> "0"}) ++ unwrap({right, pattern <> "1"})
+            x ->
+                [{x, pattern}]
+        end
     end
 end
 
