38766a50e2b8dd1377fb6a3b11117b9add10c0d0
jrreed83 <joeyrreed@gmail.com>
2017-08-25 20:41:42 -0700

    jrr

--- haskell/huffman/huffman.hs
+++ haskell/huffman/huffman.hs
@@ -1,16 +1,44 @@
 module Huffman where
-    data Tree a = Leaf a 
-                | Branch (Tree a) (Tree a) 
-                deriving (Show)
-    
-    data Node a = Node (Tree a) (Float)
-                deriving (Show)
 
-    merge :: Node a -> Node a -> Node a
+    data Tree  = Leaf String 
+               | Branch Tree Tree 
+               deriving (Show)
+    
+    data Node = Node Tree Float 
+              deriving (Show)
+
+    merge :: Node -> Node -> Node 
     merge (Node t1 p1) (Node t2 p2) 
         | p1 <= p2  = Node (Branch t1 t2) (p1+p2)
         | otherwise = Node (Branch t2 t1) (p1+p2) 
 
-    build_tree :: [(a, Float)] -> Tree a
+    probability :: Node -> Float
+    probability (Node _ p) = p
 
-    unwrap :: Tree a -> [(a, String)]
+    tree :: Node -> Tree 
+    tree (Node t _) = t 
+
+    add :: [Node] -> Node -> [Node]
+    add [] node   = [node]
+    add list node = 
+        inner list node [] 
+        where
+            inner [] node accum = accum ++ [node]
+            inner (h:t) node accum 
+                | probability h <  (probability node) = inner t node (accum ++ [h])
+                | probability h >= (probability node) = accum ++ [node] ++ (h:t)
+
+    convert :: [(String, Float)] -> [Node]
+    convert l = map (\(s,f) -> (Node (Leaf s) f)) l
+
+    build_tree :: [Node] -> Tree  
+    build_tree (h:[]) = tree h
+    build_tree list   =
+        build_tree (add t hh) 
+        where
+            (h1:h2:t) = list
+            hh = merge h1 h2
+
+
+        
+        
