3327b55f1b389ab73be4df911494b702a7af5eeb
jrreed83 <joeyrreed@gmail.com>
2017-08-28 06:09:09 -0700

    jrr

--- haskell/shannon-fano/shannon_fano.hs
+++ haskell/shannon-fano/shannon_fano.hs
@@ -1,24 +1,35 @@
 module ShannonFano where
 
+    data Node = Node {char :: Char 
+                     ,freq :: Int
+                     } deriving Show 
+
+    instance Eq Node where 
+        (==) node1 node2
+            = (freq node1) == (freq node2)
+        
+        (/=) node1 node2
+            = (freq node1) /= (freq node2) 
+
     split :: Int -> [a] -> ([a], [a])
     split n list 
         = (take n list, drop n list) 
 
-    total :: [(Char,Int)] -> Int
+    total :: [Node] -> Int
     total list 
-        = foldl (\acc node -> acc + (snd node)) 0 list 
+        = foldl (\acc node -> acc + (freq node)) 0 list 
 
-    node_list_split :: Int -> [(Char,Int)] -> ([(Char,Int)], [(Char,Int)], Int)
+    node_list_split :: Int -> [Node] -> ([Node], [Node], Int)
     node_list_split i list = 
         let (left, right) = split i list
             diff = discrepancy left right
         in (left, right, diff)
 
-    discrepancy :: [(Char,Int)] -> [(Char,Int)]  -> Int
+    discrepancy :: [Node] -> [Node]  -> Int
     discrepancy list1 list2
         = abs ((total list1) - (total list2))
 
-    all_splits :: [(Char,Int)] -> [([(Char,Int)], [(Char,Int)], Int)]
+    all_splits :: [Node] -> [([Node], [Node], Int)]
     all_splits list
         = map (\i -> node_list_split i list) [1..length(list)-1]
     
