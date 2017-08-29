9182cccc7735b91d88b4cc75c46166732a19197c
jrreed83 <joeyrreed@gmail.com>
2017-08-28 20:45:13 -0700

    jrr

--- haskell/shannon-fano/shannon_fano.hs
+++ haskell/shannon-fano/shannon_fano.hs
@@ -4,32 +4,39 @@ module ShannonFano where
                      ,freq :: Int
                      } deriving Show 
 
-    instance Eq Node where 
-        (==) node1 node2
-            = (freq node1) == (freq node2)
-        
-        (/=) node1 node2
-            = (freq node1) /= (freq node2) 
+    (|>) :: a -> (a -> b) -> b 
+    (|>) x f = f x
 
     split :: Int -> [a] -> ([a], [a])
     split n list 
         = (take n list, drop n list) 
 
-    total :: [Node] -> Int
-    total list 
-        = foldl (\acc node -> acc + (freq node)) 0 list 
-
-    node_list_split :: Int -> [Node] -> ([Node], [Node], Int)
-    node_list_split i list = 
-        let (left, right) = split i list
-            diff = discrepancy left right
-        in (left, right, diff)
-
-    discrepancy :: [Node] -> [Node]  -> Int
-    discrepancy list1 list2
-        = abs ((total list1) - (total list2))
-
-    all_splits :: [Node] -> [([Node], [Node], Int)]
-    all_splits list
-        = map (\i -> node_list_split i list) [1..length(list)-1]
+    discrepancy :: [Node] -> Int -> Int 
+    discrepancy list i
+        = abs ( m - n )
+        where
+            m = list |> (take i) |> map (\t -> freq t) |> sum
+            n = list |> (drop i) |> map (\t -> freq t) |> sum
     
+    opt_split :: [Node] -> (Int,Int)
+    opt_split list 
+        = [1 .. n] |> map (\i -> (i,fn i)) 
+                   |> foldl (\acc x -> 
+                        if (snd x) < (snd acc) then x
+                        else acc
+                    ) (-1,100)
+        where
+            n  = (length list) - 1 
+            fn = discrepancy list
+    
+    table :: [Node] -> [(Char, String)]
+    table list
+        = inner list "" 
+            where 
+                inner (h:[]) pattern = [(char h, pattern)]
+                inner (h:t ) pattern
+                    = inner (left) (pattern ++ "0") ++ inner (right) (pattern ++ "1")
+                    where
+                        (i,_) = opt_split (h:t)
+                        (left, right) = split i (h:t) 
+
